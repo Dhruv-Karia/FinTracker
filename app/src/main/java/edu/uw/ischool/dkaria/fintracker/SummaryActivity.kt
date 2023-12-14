62c6a6c078d6b9e690b946454a3841fa0bc3df1a
Dhruv Karia <dhruvkaria@hotmail.com>
2023-12-13 20:30:15 -0800

    done fully

--- app/src/main/java/edu/uw/ischool/dkaria/fintracker/SummaryActivity.kt
+++ app/src/main/java/edu/uw/ischool/dkaria/fintracker/SummaryActivity.kt
@@ -1,65 +1,120 @@
 package edu.uw.ischool.dkaria.fintracker
 
-import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import java.io.BufferedReader
-import java.io.InputStreamReader
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import java.util.*
 
-data class Transaction(val date: String, val reference: String, val out: Double)
-
+data class Transaction(val date: String, val reference: String, val out: Double) {
+    // Add an empty constructor for Firebase to deserialize the data
+    constructor() : this("", "", 0.0)
+}
 class SummaryActivity : ComponentActivity() {
+
+    private val transactions = mutableListOf<Transaction>()
+    private lateinit var database: DatabaseReference
+    private lateinit var userId: String
+    private lateinit var budgetTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_summary)
 
-        val budgetTextView = findViewById<TextView>(R.id.budget)
-        val transactionsLayout = findViewById<LinearLayout>(R.id.transactions) // Assuming you have a LinearLayout with id 'transactions' in your layout
+        // Initialize Firebase Database
+        database = FirebaseDatabase.getInstance().getReference("transactions")
+        userId = FirebaseAuth.getInstance().currentUser?.uid ?: ""
 
-        val budgetString = intent.getStringExtra("budget") // Get the budget from the previous activity
-        val budget = if (budgetString.isNullOrBlank()) 0.0 else budgetString.toDouble()
-        val uri = Uri.parse(intent.getStringExtra("uri")) // Get the Uri from the previous activity
+        budgetTextView = findViewById<TextView>(R.id.budget)
+        val transactionsLayout = findViewById<LinearLayout>(R.id.transactions)
+        val dateEditText = findViewById<EditText>(R.id.date)
+        val referenceEditText = findViewById<EditText>(R.id.reference)
+        val outEditText = findViewById<EditText>(R.id.out)
+        val addButton = findViewById<Button>(R.id.add)
 
-        val transactions = readCSV(uri) // Read the transactions from the CSV
+//        val budgetString = intent.getStringExtra("budget")
+//        val budget = if (budgetString.isNullOrBlank()) 0.0 else budgetString.toDouble()
 
-        val sum = transactions.sumOf { it.out } // Calculate the sum of all transactions
-        val remaining = budget?.minus(sum) // Subtract the sum from the budget
+        // Fetch transactions from Firebase
+        fetchTransactionsFromFirebase()
 
-        budgetTextView.text = "Remaining budget: $remaining" // Display the remaining budget
+        // Update budget initiall
 
-        // Display each transaction
         for (transaction in transactions) {
             val transactionTextView = TextView(this)
-            transactionTextView.text = "Date: ${transaction.date}, Reference: ${transaction.reference}, Out: ${transaction.out}"
+            transactionTextView.text =
+                "Date: ${transaction.date}, Reference: ${transaction.reference}, Out: ${transaction.out}"
+            transactionsLayout.addView(transactionTextView)
+        }
+
+        addButton.setOnClickListener {
+            val date = dateEditText.text.toString()
+            val reference = referenceEditText.text.toString()
+            val out = outEditText.text.toString().toDouble()
+
+            val transaction = Transaction(date, reference, out)
+            transactions.add(transaction)
+
+            // Add the transaction to Firebase
+            database.child(userId).push().setValue(transaction)
+
+            val transactionTextView = TextView(this)
+            transactionTextView.text =
+                "Date: ${transaction.date}, Reference: ${transaction.reference}, Out: ${transaction.out}"
+            transactionsLayout.addView(transactionTextView)
+
+            // Update the budget when transactions are added
+            updateBudget()
+
+            // Clear the EditText fields after adding the transaction
+            dateEditText.text.clear()
+            referenceEditText.text.clear()
+            outEditText.text.clear()
+        }
+    }
+
+    private fun fetchTransactionsFromFirebase() {
+        database.child(userId).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                transactions.clear()
+
+                for (childSnapshot in snapshot.children) {
+                    val transaction = childSnapshot.getValue(Transaction::class.java)
+                    transaction?.let { transactions.add(it) }
+                }
+
+                // Update the UI with fetched transactions
+                updateBudget()
+                updateUIWithTransactions()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle errors
+            }
+        })
+    }
+
+    private fun updateUIWithTransactions() {
+        val transactionsLayout = findViewById<LinearLayout>(R.id.transactions)
+        transactionsLayout.removeAllViews()
+
+        for (transaction in transactions) {
+            val transactionTextView = TextView(this)
+            transactionTextView.text =
+                "Date: ${transaction.date}, Reference: ${transaction.reference}, Out: ${transaction.out}"
             transactionsLayout.addView(transactionTextView)
         }
     }
 
-    private fun readCSV(uri: Uri): List<Transaction> {
-        val inputStream = contentResolver.openInputStream(uri)
-        val reader = BufferedReader(InputStreamReader(inputStream))
-
-        val transactions = mutableListOf<Transaction>()
-        var line: String? = reader.readLine() // Read the header
-        while (reader.readLine().also { line = it } != null) {
-            val tokens = line?.split(",")
-            if (tokens != null) {
-                if (tokens.size >= 3) {
-                    val date = tokens[0]
-                    val reference = tokens[1]
-                    val outString = tokens[3]
-                    val out = if (outString.isBlank()) 0.0 else outString.toDouble()
-
-                    transactions.add(Transaction(date, reference, out)) // Add the transaction to the list
-                }
-            }
-        }
-
-        reader.close()
-
-        return transactions
+    private fun updateBudget() {
+        val budgetString = intent.getStringExtra("budget")
+        val budget = if (budgetString.isNullOrBlank()) 0.0 else budgetString.toDouble()
+        val sum = transactions.sumOf { it.out }
+        val remaining = budget.minus(sum)
+        budgetTextView.text = "Remaining budget: $remaining"
     }
 }
